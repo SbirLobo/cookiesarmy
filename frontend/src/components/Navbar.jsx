872d0a93c26a6a1cbbd0978eb3afd23410eeaa67
Youcef Aissat <y.aissat@gmail.com>
2023-06-28 17:04:31 +0200

    add Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,9 +11,9 @@ function Navbar() {
         />
       </div>
       <div className="mt-[10rem] text-primary font-semibold">
-        <h2 className="mb-[4rem]  p-2">Ajouter un smartphone</h2>
-        <h2 className="mb-[4rem]  p-2">Base de données</h2>
-        <h2 className="p-2">FAQ</h2>
+        <h2 className="mb-[4rem]">Ajouter un smartphone</h2>
+        <h2 className="mb-[4rem]">Base de données</h2>
+        <h2 className="">FAQ</h2>
       </div>
       <div className="flex w-[10rem] justify-between mt-[3rem] mb-[4rem]">
         <img src="../src/assets/Vector.png" alt="vector" className="w-4 h-4" />
