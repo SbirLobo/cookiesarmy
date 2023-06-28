12812d237fd99fb365788fb979729b9f3cd02924
Youcef Aissat <y.aissat@gmail.com>
2023-06-28 16:53:40 +0200

    add Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,17 +2,21 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="flex h-[100vh] flex-col w-[15rem] justify-around items-center border-r-[2px] border-primary">
-      <div className="mt-[30px]">
-        <img src="../src/assets/logo.png" alt="logo" className="w-[10rem]" />
+    <div className="flex h-[100vh] flex-col w-[15rem] justify-between items-center border-r-[2px] border-primary">
+      <div className="">
+        <img
+          src="../src/assets/logo.png"
+          alt="logo"
+          className="w-[10rem]  mt-8"
+        />
       </div>
-      <div className="mt-[10rem]">
+      <div className="mt-[10rem] text-primary font-semibold">
         <h2 className="mb-[4rem]  p-2">Ajouter un smartphone</h2>
         <h2 className="mb-[4rem]  p-2">Base de données</h2>
         <h2 className="p-2">FAQ</h2>
       </div>
-      <div className="flex w-[10rem] justify-between mt-[10rem] mb-[30px]">
-        <img src="../src/assets/Vector.png" alt="vector" />
+      <div className="flex w-[10rem] justify-between mt-[3rem] mb-[4rem]">
+        <img src="../src/assets/Vector.png" alt="vector" className="w-4 h-4" />
         <h2>J.Doe</h2>
         <img src="../src/assets/connection.png" alt="connection" />
       </div>
