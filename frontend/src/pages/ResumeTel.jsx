1bd8f08652416d8fa61e525f1162f46617614d08
Chl0l <chloe.kim.elle@gmail.com>
2023-06-30 01:31:15 +0200

    Update creating mobile2

--- frontend/src/pages/ResumeTel.jsx
+++ frontend/src/pages/ResumeTel.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
 import { useInfoMobile } from "../contexts/InfoMobileContext";
