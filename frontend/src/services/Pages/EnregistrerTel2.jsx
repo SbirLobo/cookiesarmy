8eb651e8c9d7ab13e0e7f5809d478aa0349742ba
OyhanaM <oyhana.mahjoubi@gmail.com>
2023-06-29 10:01:33 +0200

    fix

--- frontend/src/services/Pages/EnregistrerTel2.jsx
+++ frontend/src/services/Pages/EnregistrerTel2.jsx
@@ -15,81 +15,86 @@ function EnregistrerTel2() {
       <h2 className="font-bold p-4">
         Caractéristiques générales du smartphone:
       </h2>
-      <div className="flex flex-wrap justify-between p-3 lg:px-24">
-        <div>
-          <label htmlFor="etat">Etat* : </label>
-          <br />
-          <select name="etat" className="rounded border w-36 sm:w-52  md:w-72">
-            <option value="" />
-            <option value="DEE">DEE</option>
-            <option value="réparable">Réparable</option>
-            <option value="bloqué">Bloqué</option>
-            <option value="reconditionable">Reconditionnable</option>
-            <option value="reconditionné">Reconditionné</option>
-            <option value="neuf">Neuf</option>
-          </select>
+      <form>
+        <div className="flex flex-wrap justify-between p-3 lg:px-24">
+          <div>
+            <label htmlFor="etat">Etat* : </label>
+            <br />
+            <select
+              name="etat"
+              id="etat"
+              className="rounded border w-36 sm:w-52  md:w-72"
+            >
+              <option value="na">---</option>
+              <option value="DEE">DEE</option>
+              <option value="réparable">Réparable</option>
+              <option value="bloqué">Bloqué</option>
+              <option value="reconditionable">Reconditionnable</option>
+              <option value="reconditionné">Reconditionné</option>
+              <option value="neuf">Neuf</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="aspect">Aspect* : </label>
+            <br />
+            <select
+              name="aspect"
+              id="aspect"
+              className="rounded border w-36 sm:w-52  md:w-72"
+            >
+              <option value="na">---</option>
+              <option value="mauvais">Mauvais état</option>
+              <option value="correct">Correct</option>
+              <option value="bon">Bon état</option>
+            </select>
+          </div>
         </div>
-        <div>
-          <label htmlFor="aspect">Aspect* : </label>
-          <br />
-          <select
-            name="aspect"
-            className="rounded border w-36 sm:w-52  md:w-72"
+
+        <div className="flex flex-wrap justify-between p-3 lg:px-24">
+          <div>
+            <label htmlFor="taille">Taille d'écran : </label>
+            <br />
+            <input
+              type="text"
+              name="taille"
+              id="taille"
+              className="rounded border w-36 sm:w-52  md:w-72"
+              placeholder="En pouce"
+            />
+          </div>
+          <div className="gap-5 items-center flex">
+            <label htmlFor="sim">Double Sim</label>
+            <input type="checkbox" name="sim" id="sim" />
+          </div>
+        </div>
+
+        <div className="flex flex-wrap justify-between p-3 lg:px-24">
+          <div>
+            <label htmlFor="couleur">Couleur : </label>
+            <br />
+            <input
+              type="text"
+              name="couleur"
+              id="couleur"
+              className="rounded border w-36 sm:w-52  md:w-72"
+            />
+          </div>
+          <div className="gap-5 items-center flex">
+            <label htmlFor="ecouteurs">Ecouteurs</label>
+            <input type="checkbox" name="ecouteurs" id="ecouteurs" />
+          </div>
+        </div>
+
+        <div className="flex justify-center m-5">
+          <button
+            type="button"
+            className="bg-tertiary text-quinary rounded-full w-32"
           >
-            <option value="" />
-            <option value="mauvais">Mauvais état</option>
-            <option value="correct">Correct</option>
-            <option value="bon">Bon état</option>
-          </select>
+            Suivant
+          </button>
         </div>
-      </div>
-
-      <div className="flex flex-wrap justify-between p-3 lg:px-24">
-        <div>
-          <label htmlFor="taille">Taille d'écran : </label>
-          <br />
-          <input
-            type="text"
-            name="taille"
-            className="rounded border w-36 sm:w-52  md:w-72"
-            placeholder="En pouce"
-          />
-        </div>
-        <div className="gap-5 items-center flex">
-          <label htmlFor="ecouteurs">Double Sim</label>
-
-          <input type="checkbox" name="ecouteursok" />
-          <label htmlFor="oui"></label>
-        </div>
-      </div>
-
-      <div className="flex flex-wrap justify-between p-3 lg:px-24">
-        <div>
-          <label htmlFor="couleur">Couleur : </label>
-          <br />
-          <input
-            type="text"
-            name="couleur"
-            className="rounded border w-36 sm:w-52  md:w-72"
-          />
-        </div>
-        <div className="gap-5 items-center flex">
-          <label htmlFor="ecouteurs">Ecouteurs</label>
-
-          <input type="checkbox" name="ecouteursok" />
-          <label htmlFor="oui"></label>
-        </div>
-      </div>
-
-      <div className="flex justify-center m-5">
-        <button
-          type="button"
-          className="bg-tertiary text-quinary rounded-full w-32"
-        >
-          Suivant
-        </button>
-      </div>
-      <p>* Champs obligatoires</p>
+        <p>* Champs obligatoires</p>
+      </form>
     </div>
   );
 }
--- frontend/src/services/Pages/EnregistrerTel3.jsx
+++ frontend/src/services/Pages/EnregistrerTel3.jsx
@@ -12,73 +12,79 @@ function EnregistrerTel3() {
 
       <h3 className="text-secondary font-bold p-4">3/3</h3>
       <h2 className="font-bold p-3">Elements informatifs:</h2>
-      <div className="flex flex-wrap justify-between p-3 lg:px-24">
-        <div>
-          <label htmlFor="statut">Statut* : </label>
-          <br />
-          <select name="etat" className="rounded border w-36 sm:w-52  md:w-72">
-            <option value="" />
-            <option value="vendu">Vendu</option>
-            <option value="stock">En stock</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="ville">Ville* : </label>
-          <br />
-          <input
-            type="text"
-            name="taille"
-            className="rounded border w-36 sm:w-52  md:w-72"
-          />
-        </div>
-      </div>
-
-      <div className="flex flex-wrap justify-between p-3 lg:px-24">
-        <div>
-          <label htmlFor="dateEntree">Date d'entrée : </label>
-          <br />
-          <input
-            type="text"
-            name="dateEntree"
-            placeholder="Format JJ/MM/AAAA"
-            className="rounded border w-36 sm:w-52  md:w-72"
-          />
-        </div>
-      </div>
-
-      <div className=" p-3 lg:px-24">
-        <div>
-          <label htmlFor="commentaire">Commentaire : </label>
-          <br />
-          <textarea
-            name="commentaire"
-            className="rounded border resize-none w-full"
-          />
-        </div>
-      </div>
-      <div className="p-3 lg:px-24">
-        <div>
-          <label htmlFor="photo">Importer une photo : </label>
-          <br />
-          <div className="border rounded flex-wrap flex flex-col items-center">
-            <img
-              src="../public/assets/favicon/download.png"
-              alt="Ajouter photo"
+      <form>
+        <div className="flex flex-wrap justify-between p-3 lg:px-24">
+          <div>
+            <label htmlFor="statut">Statut* : </label>
+            <br />
+            <select
+              name="etat"
+              className="rounded border w-36 sm:w-52  md:w-72"
+              id="etat"
+            >
+              <option value="vide">---</option>
+              <option value="vendu">Vendu</option>
+              <option value="stock">En stock</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="ville">Ville* : </label>
+            <br />
+            <input
+              type="text"
+              name="taille"
+              id="taille"
+              className="rounded border w-36 sm:w-52  md:w-72"
             />
-            <p className="italic text-xs">Glisser l'image pour l'importer</p>
           </div>
         </div>
-      </div>
 
-      <div className="flex justify-center m-5">
-        <button
-          type="button"
-          className="bg-tertiary text-quinary rounded-full w-32"
-        >
-          Suivant
-        </button>
-      </div>
-      <p>* Champs obligatoires</p>
+        <div className="flex flex-wrap justify-between p-3 lg:px-24">
+          <div>
+            <label htmlFor="dateEntree">Date d'entrée : </label>
+            <br />
+            <input
+              type="text"
+              name="dateEntree"
+              placeholder="Format JJ/MM/AAAA"
+              className="rounded border w-36 sm:w-52  md:w-72"
+              id="dateEntree"
+            />
+          </div>
+        </div>
+
+        <div className=" p-3 lg:px-24">
+          <div>
+            <label htmlFor="commentaire">Commentaire : </label>
+            <br />
+            <textarea
+              name="commentaire"
+              className="rounded border resize-none w-full"
+              id="commentaire"
+            />
+          </div>
+        </div>
+        <div className="p-3 lg:px-24">
+          <div>
+            <label htmlFor="photo">Importer une photo : </label>
+            <br />
+            <div className="border rounded flex-wrap flex flex-col items-center">
+              <img src="../public/assets/favicon/download.png" alt="Ajouter" />
+              <p className="italic text-xs">Glisser l'image pour l'importer</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-center m-5">
+          <button
+            type="button"
+            className="bg-tertiary text-quinary rounded-full w-32"
+          >
+            Suivant
+          </button>
+        </div>
+        <p>* Champs obligatoires</p>
+      </form>
     </div>
   );
 }
