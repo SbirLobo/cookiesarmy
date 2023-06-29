efc8cfc8357437e0667b2af21271105133607988
Youcef Aissat <y.aissat@gmail.com>
2023-06-29 13:22:39 +0200

    add Page404

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./index.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Page404 from "./pages/Page404";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 }
