abaa9297da94754298bf9cc81ab29ca4a56a7516
OyhanaM <oyhana.mahjoubi@gmail.com>
2023-06-30 03:24:19 +0200

    fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useInfoUser } from "../contexts/InfoUserContext";
 
 function Navbar() {
   const { loggedInUser, handleClickLogOut } = useInfoUser();
@@ -10,33 +11,42 @@ function Navbar() {
             <img
               src="../src/assets/logo.png"
               alt="logo"
-              className="w-[5rem] mt-8"
+              className="mt-8 w-40"
             />
           </Link>
         </div>
-        <div className="mt-[10rem] w-[8rem] text-primary font-semibold">
-          <Link to="/enregistrer1">
-            <h2 className="mb-[4rem] text-xs">Ajouter un smartphone</h2>
+        <div className="w-[8rem] text-primary h-1/3 flex flex-col items-center justify-around gap-4 font-semibold">
+          <Link to="/enregistrer1" className="w-full text-center">
+            <h2 className="text-xs py-3 p-1 my-4 hover:text-primary hover:bg-tertiary hover:rounded-full">
+              Ajout mobile
+            </h2>
           </Link>
-          <Link to="/TableauSmartphones">
-            <h2 className="mb-[4rem] text-xs">Base de données</h2>
+          <Link to="/TableauSmartphones" className="w-full text-center">
+            <h2 className="text-xs py-3 p-1 my-4 hover:text-quinary hover:bg-secondary hover:rounded-full">
+              Base de données
+            </h2>
           </Link>
-          <Link to="/FAQ">
-            <h2 className="text-xs">FAQ</h2>
+          <Link to="/FAQ" className="w-full text-center">
+            <h2 className="text-xs py-3 p-1 my-4 hover:text-primary hover:bg-quaternary hover:rounded-full">
+              FAQ
+            </h2>
           </Link>
         </div>
-        <div className="flex w-[8rem] justify-between mt-[3rem] mb-[4rem]">
+        <div className="flex w-[8rem] justify-between items-center mb-[4rem]">
           <img
             src="../src/assets/Vector.png"
             alt="vector"
             className="w-4 h-4"
           />
-          <h2 className="text-xs">J.Doe</h2>
-          <img src="../src/assets/connection.png" alt="connection" />
+          <h2 className="text-xs">
+            {loggedInUser !== "" ? loggedInUser : "Déconnecté"}
+          </h2>
+          <button type="button" onClick={handleClickLogOut}>
+            <img src="../src/assets/connection.png" alt="connection" />
+          </button>
         </div>
       </div>
     </div>
   );
 }
-
 export default Navbar;
